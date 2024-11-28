45e4a1b10462ef0d7332dbea76fcb21fef28ca2e
jammy <920817290@qq.com>
2024-11-28 10:34:10 +0800

    1.初始化提交

---
+++ test_monitor/include/monitor/monitor_inter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+#include "monitor_info.grpc.pb.h"
+namespace monitor {
+class MonitorInter {
+ public:
+  MonitorInter() {}
+  virtual ~MonitorInter() {}
+  virtual void UpdateOnce(monitor::proto::MonitorInfo* monitor_info) = 0;
+  virtual void Stop() = 0;
+};
+}  // namespace monitor
